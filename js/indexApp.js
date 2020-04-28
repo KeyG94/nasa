f6072e8af3acef8ac14070afddcebc6dbf71fdc0
Glenn Key <glennkey78@live.no>
2020-04-28 20:58:00 +0200

    added todays day to the function

--- js/indexApp.js
+++ js/indexApp.js
@@ -8,13 +8,26 @@ function gmt_clock() {
     let hours = time.getUTCHours();
     let min = time.getUTCMinutes();
     let sec = time.getUTCSeconds();
+    let date = time.getUTCDay();
+    
+    let day = new Array();
+    day[0] = "Sun";
+    day[1] = "Mon";
+    day[2] = "Tue";
+    day[3] = "Wed";
+    day[4] = "Thu";
+    day[5] = "Fri";
+    day[6] = "Sat";
+
+    let today = day[date];
     //add a 0 in front of number if its less than 2 digits
     hours = ('0' + hours).slice(-2);
     min = ('0' + min).slice(-2);
     sec = ('0' + sec).slice(-2);
+
     //output to document
     document.getElementById('gmt').innerHTML = 
-        'GMT: ' + hours + ':' + min + ':' + sec;
+        'GMT: ' + hours + ':' + min + ':' + sec + ' ' + today;
     //tick 2 times every second for accurate timer
     setTimeout(gmt_clock, 500);
 }
