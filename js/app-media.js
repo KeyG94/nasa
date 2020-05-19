aa02c120134831469681b32265e49c8a4936521c
Glenn Key <glennkey78@live.no>
2020-05-19 18:39:18 +0200

    added script to load launches and landings in reverse order

--- js/app-media.js
+++ js/app-media.js
@@ -38,8 +38,9 @@ async function getImageLibary(query) {
 
 		for (let item of data.collection.items) {
 			const image = item.links[0].href;
+			const description = item.data[0].description;
 
-			getImageDom.innerHTML += `<img src="${image}" width="150px" alt="">`;
+			getImageDom.innerHTML += `<img src="${image}" class="feed-images" alt="${description}">`;
 		}
 	});
 }
@@ -72,51 +73,100 @@ function toggleClick(clicked) {
 	}
 }
 
+window.addEventListener('load', getPictureOfDay);
 
-window.addEventListener("load", getPictureOfDay);
+async function getPictureOfDay() {
+	const latestContent = document.getElementById('apod-wrap');
+	latestContent.innerHTML = 'Loading...';
+	await fetch(apodFeed + key).then((res) => res.json()).then((data) => {
+		latestContent.innerHTML = '<h2>Picture of the day</h2>';
 
-async function getPictureOfDay(){
-    
-    const latestContent = document.getElementById('apod-wrap');
-    latestContent.innerHTML = 'Loading...'
-    await fetch(apodFeed + key).then((res)=> res.json()).then((data) => {
-     
-        latestContent.innerHTML = '<h2>Picture of the day</h2>'
+		const title = document.createElement('h3');
+		const copyright = document.createElement('div');
+		const img = document.createElement('img');
 
-        const title = document.createElement('h3');
-        const copyright = document.createElement('div');
-        const img = document.createElement('img');
+		// console.log(data)
 
-        title.innerHTML = data.title;
-        img.src = data.url
-        copyright.innerHTML = data.copyright;
-        img.alt = data.explanation;
+		title.innerHTML = data.title;
+		img.src = data.url;
+		copyright.innerHTML = data.copyright;
+		img.alt = data.explanation;
 
-        latestContent.appendChild(img);
-        latestContent.appendChild(title);
-        latestContent.appendChild(copyright);
-       
-    });
+		latestContent.appendChild(img);
+		latestContent.appendChild(title);
+		latestContent.appendChild(copyright);
+	});
 }
 
-// async function getVideoLibary(query) {
-// 	query = searchInput.value + '&media_type=video';
-// 	getImageDom.innerHTML = 'Loading...';
+//latest feed from spaceX - Launches
+const timeLineButton = document.getElementById('timeline-button');
+const spacexUrl = 'https://api.spacexdata.com/v3/launches';
+let launchesParagraph = document.getElementById('timeline');
 
-// 	await fetch(baseUrl + query).then((res) => res.json()).then((data) => {
-// 		console.log(data);
+timeLineButton.addEventListener('click', getLaunches);
 
-// 		getImageDom.innerHTML = '';
+async function getLaunches() {
+	launchesParagraph.innerHTML = '<p>Loading...</p>';
 
-// 		//handle error
-// 		if (data.collection.items.length === 0) {
-// 			getImageDom.innerHTML = `<p class="neg-feedback">No results matching your search (${searchInput.value}).</p>`;
-// 		}
+	await fetch(spacexUrl).then((res) => res.json()).then((data) => {
+        launchesParagraph.innerHTML = '';
+        data = data.reverse();
+        console.log(data)
+		for (let i = 0; i < data.length; i++) {
+			//changeble data
+			let image = data[i].links.mission_patch_small;
+			let more = data[i].links.article_link;
+			let details = data[i].details;
 
-// 		for (let item of data.collection.items) {
-// 			const image = item.links[0].href;
+			//constant data
+			const date = data[i].launch_date_utc.slice(0, 10);
+			const missionName = data[i].mission_name;
+			const rocketName = data[i].rocket.rocket_name;
+			const launchSite = data[i].launch_site.site_name_long;
 
-// 			getImageDom.innerHTML += `<img src="${image}" width="150px" alt="">`;
-// 		}
-// 	});
-// }
+			//if image patch empty
+			if (image === null) {
+				// return placeholder
+				image = 'https://lakelandescaperoom.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg';
+			}
+
+			//if article link is null
+			if (more === null) {
+				//return nasa launches page
+				more = 'https://www.nasa.gov/launchschedule/';
+			}
+
+			//if details is null
+			if (details === null) {
+				//return standard line
+				details = 'No details available for this mission';
+			}
+
+			//output data to cards
+			launchesParagraph.innerHTML += `
+            <li class="list-item">
+                <a href="${more}"><img class="card-image" src="${image}" alt="launch patch"></a>
+                <div class="left-box">
+                    <h6 class="launch-date">Date: ${date}</h6>
+                    <h5 class="mission-name">Mission: ${missionName}</h5>
+                    <p class="details">Description: ${details}</p>    
+                </div>
+                <div class="right-box">
+                    <h6 class="rocket-name">${rocketName}</h6>
+                    <h6 class="launch-site">${launchSite}</h6>
+                    <a target="_blank" href="${more}">Learn More</a>
+                </div>
+            </li>
+            `;
+
+			// console.log(image);
+			// console.log(date);
+			// console.log(more);
+			// console.log('mission name: ' + missionName);
+			// console.log('rocket name: ' + rocketName);
+			// console.log(details)
+			// console.log('Launch Site: ' + launchSite)
+		}
+	});
+}
+getLaunches();
